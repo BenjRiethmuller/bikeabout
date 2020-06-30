90676e74070833d1ff95c367bfb6ce5406aff171
Benj Riethmuller <benjriethmuller@gmail.com>
2020-06-30 14:52:25 +0200

    added three featured bikes and how it works section

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,6 +86,12 @@ Rails.application.configure do
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
 
+  # to make svg files work
+  config.assets.precompile += %w( '.svg' )
+
+  # Must include to get inline SVGs to work in deploy
+  config.assets.css_compressor = :sass
+
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
