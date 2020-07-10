bf0d9f4f2b2229d9f5a5938c2063f12a7ac8553d
Benj Riethmuller <benjriethmuller@gmail.com>
2020-07-10 17:29:39 +0200

    dashboard cards, need to change controller methods to display booked bikes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,4 +2,12 @@ class PagesController < ApplicationController
   def home
     @bikes = Bike.all
   end
+
+  def dashboard
+    @bikes = Bike.where(user_id: current_user)
+    @bookings = Booking.where(user_id: current_user)
+    # @booked_bikes = Bike.where(user_id: current_user) && Bike.where(:status == "Pending")
+    @upcoming_bookings = Booking.where('start_date >= ?', Date.today)
+    @past_bookings = Booking.where('end_date <= ?', Date.today)
+  end
 end
