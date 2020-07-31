79d1712ba79cc83a6912c55306073e7c22ba7899
Benj Riethmuller <benjriethmuller@gmail.com>
2020-07-31 18:21:13 +0200

    removed unnecesary routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   get 'dashboard', to: 'pages#dashboard', as: :dashboard
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :bikes do
-    resources :bookings, only: [:create, :show, :index]
+    resources :bookings, only: [:create]
     resources :reviews, only: [:new, :create]
   end
   resources :bookings, only: [:destroy, :update]
