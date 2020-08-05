e942459aa82166b7d907733d00d65350531e68e6
Benj Riethmuller <benjriethmuller@gmail.com>
2020-08-05 13:51:21 +0200

    fized map size, zoom, and if no bike found in a city

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -1,36 +1,31 @@
 import mapboxgl from 'mapbox-gl';
 
 const initMapbox = () => {
+  const mapElement = document.getElementById('map');
 
-const mapElement = document.getElementById('map');
+  if (mapElement) {
 
-if (mapElement) {
+    const fitMapToMarkers = (map, markers) => {
+      const bounds = new mapboxgl.LngLatBounds();
+      markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
+      map.fitBounds(bounds, {padding: 100, maxZoom: 12 });
+    };
 
-const buildMap = () => {
-  mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
-  return new mapboxgl.Map({
-    container: 'map',
-    style: 'mapbox://styles/benjriethmuller/ckc4xh5wj156d1in4z6833o1s'
-  });
-};
+    mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
 
-const addMarkersToMap = (map, markers) => {
-  markers.forEach((marker) => {
-    new mapboxgl.Marker()
+    const map = new mapboxgl.Map({
+      container: 'map',
+      style: 'mapbox://styles/benjriethmuller/ckc4xh5wj156d1in4z6833o1s',
+      center: [11.5820, 48.1351],
+      zoom: 4,
+    });
+    const markers = JSON.parse(mapElement.dataset.markers);
+    markers.forEach((marker) => {
+      new mapboxgl.Marker()
       .setLngLat([ marker.lng, marker.lat ])
       .addTo(map);
-  });
-};
+    });
 
-const fitMapToMarkers = (map, markers) => {
-  const bounds = new mapboxgl.LngLatBounds();
-  markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
-  map.fitBounds(bounds, { padding: 70, maxZoom: 15 });
-};
-
-    const map = buildMap();
-    const markers = JSON.parse(mapElement.dataset.markers);
-    addMarkersToMap(map, markers);
     fitMapToMarkers(map, markers);
   }
 };
